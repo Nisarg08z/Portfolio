5bf36fdbe713687a740ac985ac42cced70c910af
Nisarg08z <nisarg693z@gmail.com>
2025-06-17 23:23:20 +0530

    day-1.8

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,45 +1,52 @@
-import { FaJs, FaReact, FaNodeJs, FaGit, FaDocker } from "react-icons/fa";
-import { SiTailwindcss, SiMongodb, SiTypescript, SiFigma, SiPython } from "react-icons/si";
+import {
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaGit,
+  FaDocker,
+  FaPhp,
+  FaHtml5,
+  FaCss3Alt,
+  FaGithub
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiMongodb,
+  SiFigma,
+  SiPython,
+  SiMysql,
+  SiStreamlit,
+  SiOpenai,
+  SiLangchain,
+  SiCloudinary ,
+  SiPostman 
+} from "react-icons/si";
+import { BsDatabaseFillGear } from "react-icons/bs";
+import { TbBrandCpp } from "react-icons/tb";
+
 
 export const skills = [
-  {
-    name: "JavaScript",
-    icon: FaJs,
-  },
-  {
-    name: "React",
-    icon: FaReact,
-  },
-  {
-    name: "Node.js",
-    icon: FaNodeJs,
-  },
-  {
-    name: "Tailwind",
-    icon: SiTailwindcss,
-  },
-  {
-    name: "MongoDB",
-    icon: SiMongodb,
-  },
-  {
-    name: "Python",
-    icon: SiPython,
-  },
-  {
-    name: "Docker",
-    icon: FaDocker,
-  },
-  {
-    name: "Git",
-    icon: FaGit,
-  },
-  {
-    name: "TypeScript",
-    icon: SiTypescript,
-  },
-  {
-    name: "Figma",
-    icon: SiFigma,
-  },
+  { name: "C++", icon: TbBrandCpp },
+  { name: "JavaScript", icon: FaJs },
+  { name: "React", icon: FaReact },
+  { name: "Node.js", icon: FaNodeJs },
+  { name: "Express.js", icon: FaNodeJs },
+  { name: "Tailwind CSS", icon: SiTailwindcss },
+  { name: "MongoDB", icon: SiMongodb },
+  { name: "Python", icon: SiPython },
+  { name: "LangChain", icon: SiLangchain },
+  { name: "LangGraph", icon: SiLangchain },
+  { name: "OpenAI", icon: SiOpenai },
+  { name: "Streamlit", icon: SiStreamlit },
+  { name: "Qdrant", icon: BsDatabaseFillGear },
+  { name: "Postman", icon: SiPostman  },
+  { name: "Docker", icon: FaDocker },
+  { name: "Git", icon: FaGit },
+  { name: "GitHub", icon: FaGithub },
+  { name: "Figma", icon: SiFigma },
+  { name: "HTML5", icon: FaHtml5 },
+  { name: "CSS3", icon: FaCss3Alt },
+  { name: "PHP", icon: FaPhp },
+  { name: "MySQL", icon: SiMysql },
+  { name: "Cloudinary", icon: SiCloudinary  },
 ];
